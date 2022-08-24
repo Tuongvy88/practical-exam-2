2529fc7915e61e3dc6e19e85dfbd1e06d562ed42
Tuongvy88 <109933390+Tuongvy88@users.noreply.github.com>
2022-08-24 14:15:16 +0930

    Add files via upload

--- function-1-1.cpp
+++ function-1-1.cpp
@@ -1,23 +1,16 @@
 #include <iostream>
 using namespace std;
-
-void print_ascending(int *vals, int len);
-{
-    // takes an array of integers: val
-    //takes length: len
-    //prints out elements of vals (separated by spaces) until current element is smaller than previous element 
-    //or until reach end of val */
-
-    int max_value = *vals[0];
-
-    cout << max_value << ' ';
-
-    for (int i=1; i<len; i++) {
-        if (max_value < vals[i]) {
-            max_value = vals[i];
-            cout << max_value << ' ';
-        }
-    }
-
-    cout << endl;
+// declare a function
+void print_ascending(int *vals, int len){
+// access the array using for loop
+for(int i=0; i<len-1; i++){
+// if right element is greater than left element the break the loop
+if(vals[i]>vals[i+1]){
+break;
+}
+// otherwise
+else{
+cout<<vals[i]<<" ";
+}
+}
 }
